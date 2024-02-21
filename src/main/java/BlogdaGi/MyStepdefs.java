afadbc0200d63f1abdaee88f5d7b9f7911029613
Rafaela Scalabrini Oliveira <49128987+RafaelaScalabrini@users.noreply.github.com>
2024-02-21 16:47:49 -0300

    Update MyStepdefs.java

--- src/main/java/BlogdaGi/MyStepdefs.java
+++ src/main/java/BlogdaGi/MyStepdefs.java
@@ -3,7 +3,6 @@ package BlogdaGi;
 import Pages.Inscrever;
 import Pages.Noticia;
 import Pages.Pesquisa;
-import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
@@ -11,6 +10,9 @@ import io.cucumber.java.pt.Quando;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
 
 public class MyStepdefs {
     protected static WebDriver driver;
@@ -28,6 +30,7 @@ public class MyStepdefs {
         System.setProperty("webdriver.chrome.driver", CAMINHO_DRIVER);
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get(URL_BASE);
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
@@ -37,59 +40,63 @@ public class MyStepdefs {
         driver = new ChromeDriver(options);
 
     }
-    public MyStepdefs() {
+
+    @Dado("que o usuário está na página inicial do site do blog do Agi: https:\\/\\/blogdoagi.com.br\\/")
+    public void queOUsuárioEstáNaPáginaInicialDoSiteDoBlogDoAgiHttpsBlogdoagiComBr() {
+
+
+        driver.get(URL_BASE);
+    }
+
+    @Quando("o usuário seleciona uma notícia específica no menu do Agibank {string}")
+    public void oUsuárioSelecionaUmaNotíciaEspecíficaNoMenuDoAgibank(String arg0) {
+
+        Noticia noticia = new Noticia(driver);
+        noticia.selectnoticia();
 
     }
-        @Dado("^que o usuário está na página inicial do aplicativo$")
-            public void que_o_usuário_está_na_página_inicial_do_aplicativo(){
-
-                noticia = new Noticia(driver);
-            }
 
 
+    @Então("a notícia selecionada é exibida em tela cheia com detalhes {string}")
+    public void aNotíciaSelecionadaÉExibidaEmTelaCheiaComDetalhes(String arg0) {
 
-        @Quando("^o usuário seleciona uma notícia específica no menu do Agibank$")
-            public void o_usuário_seleciona_uma_notícia_específica_no_menu_do_Agibank(){
-                noticia.selectnoticia();
+    }
 
-            }
+    @Dado("que o usuário está na página inicial do site do blog do Agi: https:\\/\\/blogdoagi.com.br\\/")
+    public void queOUsuárioEstáNaPáginaInicialDoSiteDoBlogDoAgiHttpsBlogdoagiComBr() {
+        driver.get(URL_BASE);
+
+    }
+
+    @Quando("o usuário realiza uma pesquisa por uma {string}")
+    public void oUsuárioRealizaUmaPesquisaPorUma(String arg0) {
+        pesquisa = new Pesquisa(driver);
+        pesquisa.Pesquisa();
+    }
 
 
-                @Então("^a notícia selecionada é exibida em tela cheia com detalhes$")
-                    public void a_notícia_selecionada_é_exibida_em_tela_cheia_com_detalhes(){
-                        noticia.selectnoticia();
+    @Então("uma lista de resultados relacionados à pesquisa {string}")
+    public void umaListaDeResultadosRelacionadosÀPesquisa(String arg0) {
+    }
 
-                    }
+    @Dado("que o usuário está na página inicial do Blog do Agi :{string}")
+    public void queOUsuárioEstáNaPáginaInicialDoBlogDoAgi(String arg0) {
+    }
 
-        @Dado("^que o usuário está na página inicial do site do blog do Agi: https://blogdoagi\\.com\\.br/$")
-            public void que_o_usuário_está_na_página_inicial_do_site() {
-                pesquisa = new Pesquisa(driver);
-            }
+    @Quando("o usuário insere seu endereço de e-mail na caixa de inscrição de newsletter e clica no botão {string}")
+    public void oUsuárioInsereSeuEndereçoDeEMailNaCaixaDeInscriçãoDeNewsletterEClicaNoBotão(String arg0) {
+        inscrever = new Inscrever(driver);
+    }
 
-        @Quando("^o usuário realiza uma pesquisa por uma carreira$")
-            public void o_usuário_realiza_uma_pesquisa_por_uma_arreira() {
-                pesquisa.Pesquisa();
-            }
-        @Então("^uma lista de resultados relacionados à pesquisa é exibida$")
-            public void uma_lista_de_resultados_relacionados_à_pesquisa_é_exibida() {
-                pesquisa.Pesquisa();
-            }
-        @Dado("^que o usuário está na página inicial do Blog do Agi$")
-            public void que_o_usuário_está_na_página_inicial() {
-            inscrever = new Inscrever(driver);
-        }
-        @Quando("^o usuário insere seu endereço de e-mail na caixa de inscrição de newsletter e clica no botão \"([^\"]*)\"$")
-            public void o_usuário_insere_seu_endereço_de_e_mail() {
-                inscrever.fillOutTheInscrever();
-        }
-       @Então("^o usuário recebe uma mensagem de confirmação de inscrição e é adicionado à lista de assinantes da newsletter$")
-           public void o_usuário_recebeumamensagem() {
-               inscrever.fillOutTheInscrever();
-           }
 
-           @After()
-           public void closeBrowser() {
-               driver.quit();
-           }
+    @Então("o usuário recebe uma mensagem de confirmação de inscrição e é adicionado à lista de assinantes da {string}")
+    public void oUsuárioRecebeUmaMensagemDeConfirmaçãoDeInscriçãoEÉAdicionadoÀListaDeAssinantesDa(String arg0) {
+    }
 
-           }
+
+    @After()
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+}
