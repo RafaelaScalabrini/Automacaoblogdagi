593d97abdbd946354e952624c5650261965d5e74
Rafaela Scalabrini Oliveira <49128987+RafaelaScalabrini@users.noreply.github.com>
2024-02-21 16:46:58 -0300

    Update Noticia.java

--- src/main/java/Pages/Noticia.java
+++ src/main/java/Pages/Noticia.java
@@ -2,6 +2,7 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Noticia extends BasePage{
     public Noticia(WebDriver driver) {
@@ -11,14 +12,21 @@ public class Noticia extends BasePage{
 
 
     //Campos (Locators = XPath)
+
+    public String agibank =  "menu-item-3713";
+    public String noticia = "menu-item-3718";
+
+
    protected String Agibank =  "//li[@id='menu-item-3713']";
     protected String Noticia = "//li[@id='menu-item-3718']";
+
 
     //Metodos para executar uma ação em algum campo definido acima
     public void selectnoticia () {
 
-        click(driver.findElement(By.xpath(Agibank)));
-        click(driver.findElement(By.xpath(Noticia)));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(By.id(agibank))).perform();
+        click(driver.findElement(By.id(noticia)));
 
     }
 
